38160dbd8e371b972000a71ccc83b468d585d92c
byxbe <byxbe@10.7.1.140>
2026-03-24 20:28:41 -0700

    This is the first commit

---
+++ YourSJSU/src/main/java/MysqlCon.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+class MysqlCon{
+	public static void main(String args[]){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");  
+			String username = "root";
+			String password = "Password:)#1";
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/wong?autoReconnect=true&useSSL=false", username, password);
+			
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("select * from Student");
+			while(rs.next())
+				System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+				con.close();
+		} catch(Exception e) {
+			System.out.println(e);
+			
+		}
+	
+	}
+}
